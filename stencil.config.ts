03999791f2adfff177a57818622888940ace62f9
danirobles03@gmail.com <danirobles03@gmail.com>
2025-11-07 16:46:41 +0100

    segundo del todo xd

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,17 +1,14 @@
 import { Config } from '@stencil/core';
 
-// https://stenciljs.com/docs/config
-
 export const config: Config = {
-  globalStyle: 'src/global/app.css',
-  globalScript: 'src/global/app.ts',
-  taskQueue: 'async',
+  namespace: 'start-cleaner',
   outputTargets: [
+    { type: 'dist' },
+    { type: 'docs' },
     {
       type: 'www',
-      // comment the following line to disable service workers in production
       serviceWorker: null,
-      baseUrl: 'https://myapp.local/',
     },
   ],
 };
+
